995ead05d08511088ffb77c584c3a11396998120
chjasp <ch.jasper1@gmail.com>
2025-10-04 15:48:15 +0200

    auth cleanup

--- Auth-Broker/02-infra/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "project_id"        { type = string }
-variable "region"            { type = string }
-variable "service_name"      { type = string }
-variable "image_url"         { type = string }
-
-variable "whoop_client_id"     { type = string }
-variable "whoop_client_secret" { type = string }
-variable "broker_api_key"      { type = string }
